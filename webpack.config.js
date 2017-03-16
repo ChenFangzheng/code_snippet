666e1f8907e63820ab815e7af4449d8432909b6b
fangzheng <chenfangzheng@outlook.com>
2017-03-16 17:51:28 +0800

    rename folder

--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,9 @@ var TEM_PATH = path.resolve(APP_PATH, '');
 
 module.exports = {
     entry: {
-        shishijiance: path.resolve(APP_PATH, './0.shishijiance/index.js'),
-        zhengcemoni: path.resolve(APP_PATH, './1.zhengcemoni/index.js'),
-        wanquanjiance: path.resolve(APP_PATH, './2.wanquanjiance/index.js'),
+        shishijiance: path.resolve(APP_PATH, './d3v3/index.js'),
+        zhengcemoni: path.resolve(APP_PATH, './es6test/index.js'),
+        wanquanjiance: path.resolve(APP_PATH, './highcharts/index.js'),
         vendors: ['jquery', 'highcharts', 'd3', 'lodash', 'babel-polyfill']
     },
     output: {
@@ -63,21 +63,21 @@ module.exports = {
         new ExtractTextPlugin('[name].css', { allChunks: true }),
         new HtmlwebpackPlugin({
             title: '实时监测',
-            template: path.resolve(TEM_PATH, './0.shishijiance/index.html'),
+            template: path.resolve(TEM_PATH, './d3v3/index.html'),
             filename: 'index.html',
             chunks: ['shishijiance', 'vendors'],
             inject: 'body'
         }),
         new HtmlwebpackPlugin({
             title: '政策模拟',
-            template: path.resolve(TEM_PATH, './1.zhengcemoni/index.html'),
+            template: path.resolve(TEM_PATH, './es6test/index.html'),
             filename: 'zhengcemoni.html',
             chunks: ['zhengcemoni', 'vendors'],
             inject: 'body'
         }),
         new HtmlwebpackPlugin({
             title: '完全监测',
-            template: path.resolve(TEM_PATH, './2.wanquanjiance/index.html'),
+            template: path.resolve(TEM_PATH, './highcharts/index.html'),
             filename: 'wanquanjiance.html',
             chunks: ['wanquanjiance', 'vendors'],
             inject: 'body'
