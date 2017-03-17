ade950b7e2eef025642adf811a2c0f22eaabe1ee
fangzheng <chenfangzheng@outlook.com>
2017-03-17 09:15:51 +0800

    clean the js files

--- src/d3v3/index.js
+++ src/d3v3/index.js
@@ -1,23 +1,6 @@
 
 import './index.scss';
-import topNav from '../components/rightNav';
-import TimeLineMonth from '../components/timeLineMonth';
 import WaveChart from './components/waveChart';
-
-const timeLine = new TimeLineMonth('#timeLine', update);
-timeLine.render({ year: 2017, month: 3 });
 
 const waveChart = new WaveChart('waveChart');
 waveChart.render();
-
-window.setTest = function () {
-    waveChart.changeDataShow(1);
-}
-
-function update(monthIndex) {
-
-    console.log(monthIndex);
-}
-
-const topnav = new topNav("menu", "nav_ss.png");
-topnav.render();
